1104ec35e8a022113cd09367c8815f58225c6c78
Mr. AtiX <1644259+mratix@users.noreply.github.com>
2026-02-14 03:08:09 +0100

    Optimize Kodi musicvideo NFO generator and fix XML escaping

--- audio-video/kodi_nfo-generator_musicvideos.sh
+++ audio-video/kodi_nfo-generator_musicvideos.sh
@@ -7,6 +7,30 @@ version="250101, by Mr.AtiX"
 # ============================================================
 
 set -euo pipefail
+shopt -s nullglob
+
+trim_and_fold_spaces() {
+    local s="$1"
+    s=${s//$'\t'/ }
+    s=${s//$'\r'/ }
+    s=${s//$'\n'/ }
+    while [[ "$s" == *"  "* ]]; do
+        s=${s//  / }
+    done
+    s="${s#"${s%%[![:space:]]*}"}"
+    s="${s%"${s##*[![:space:]]}"}"
+    printf '%s' "$s"
+}
+
+xml_escape() {
+    local s="$1"
+    s=${s//&/\&amp;}
+    s=${s//</\&lt;}
+    s=${s//>/\&gt;}
+    s=${s//\"/\&quot;}
+    s=${s//\'/\&apos;}
+    printf '%s' "$s"
+}
 
 conv_mkv(){
 # convert mkv to mp4
@@ -37,49 +61,49 @@ return $?
 
 for infile in *.mp4; do
 	# prepare
-    echo "input : "${infile}
-    outfile=$(echo ${infile} | xargs -0)			# clean whitespaces and misc chars in filename
-    echo "rename: "${outfile}
-#	mv -u ${infile} ${outfile} && infile=${outfile}	# rename file
-		#input :  Eisbrecher - Volle Kraft voraus (Live, Circus Krone) .mp4
-		#        ^                                                    ^	<---
-		#rename: Eisbrecher - Volle Kraft voraus (Live, Circus Krone) .mp4
-		#                                                            ^	<--- error on trailing space
-		#mv: das angegebene Ziel '.mp4' ist kein Verzeichnis
-		#mv: target 'Remix).mp4' is not a directory
-    nfofile=${infile%.*}.nfo
+    echo "input : ${infile}"
+    outfile="$infile"                               # keep original filename unless explicit rename is enabled
+    echo "rename: ${outfile}"
+    nfofile="${infile%.*}.nfo"
     artist=${infile%% - *}; title=${infile#* - }	# extract artist and title from filename
-    artist=$(echo $artist | xargs -0)               # clean whitespaces
-    title=$(echo $title | xargs -0)                 # clean whitespaces
+    artist="$(trim_and_fold_spaces "$artist")"      # clean whitespaces
+    title="$(trim_and_fold_spaces "$title")"        # clean whitespaces
     title=${title##*/}                              # cut extension from title
     title=${title%.mp4}                             # cut extension from title
     echo "artist: $artist"
     echo "title : $title"
 
-	if (echo "$title" | grep -iw "Audio only"); then conv_mp3; fi # convert to mp3 and delete videofile
+    shopt -s nocasematch
+	if [[ "$title" =~ (^|[[:space:]])audio[[:space:]]+only($|[[:space:]]) ]]; then conv_mp3; fi # convert to mp3 and delete videofile
 
     # construct the .nfo-file
-    echo "create:" ${nfofile}
-    echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>' > ${nfofile}    # header
-    echo "<!-- created $(date +"%Y-%m-%d %H:%M:%S") with $(basename $0) -->" >> ${nfofile}  # comment
-    echo "<musicvideo>" >> ${nfofile}                   # The top level parent tag, class of media
+    echo "create: ${nfofile}"
+    {
+        echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>'
+        echo "<!-- created $(date +"%Y-%m-%d %H:%M:%S") with $(basename "$0") -->"
+        echo "<musicvideo>"
+    } > "$nfofile"
     if [ -z "$title" ]; then
-		echo "  <title>${infile%.mp4}</title>" >> ${nfofile} # title of music video, simple filename
+        safe_title="$(xml_escape "${infile%.mp4}")"
+		echo "  <title>${safe_title}</title>" >> "$nfofile" # title of music video, simple filename
     else
-        echo "  <title>${title}</title>" | sed -r 's/[[:blank:]]+/ /g' >> ${nfofile}  # title of music video, extracted title
+        safe_title="$(xml_escape "$title")"
+        echo "  <title>${safe_title}</title>" >> "$nfofile"     # title of music video, extracted title
     fi
-	echo "  <userrating></userrating>" >> ${nfofile}    # Personal rating applied by the user
-	echo "  <album></album>" >> ${nfofile}              # Name of album the song appears on
-	echo "  <plot></plot>" >> ${nfofile}                # Review / information of music video
-	echo "  <runtime></runtime>" >> ${nfofile}          # Minutes only. If ommitted, Kodi will add runtime upon scanning
+    {
+	    echo "  <userrating></userrating>"               # Personal rating applied by the user
+	    echo "  <album></album>"                         # Name of album the song appears on
+	    echo "  <plot></plot>"                           # Review / information of music video
+	    echo "  <runtime></runtime>"                     # Minutes only. If ommitted, Kodi will add runtime upon scanning
 #	echo         title=(echo -n) $title"  <thumb aspect="thumb" preview=""></thumb>"  # can multiple, Path to TV Show and Season artwork
 #	echo "  <playcount></playcount>" >> ${nfofile}      # Setting this to 1 or greater will mark the Music Video
 #   echo "  <lastplayed></lastplayed>" >> ${nfofile}    # Date last played, Format as yyyy-mm-dd
-	echo "  <genre></genre>" >> ${nfofile}              # can multiple, Genre
-    echo "  <tag>musicvideo</tag>" >> ${nfofile}        # can multiple, Video tags
-    if (echo "$title" | grep -iw "Live"); then echo "  <tag>live</tag>" >> ${nfofile}; fi
-    if (echo "$title" | grep -iq "Lyric"); then echo "  <tag>lyrics</tag>" >> ${nfofile}; fi
-    if (echo "$title" | grep -iw "Remix"); then echo "  <tag>remix</tag>" >> ${nfofile}; fi
+	    echo "  <genre></genre>"                         # can multiple, Genre
+        echo "  <tag>musicvideo</tag>"                  # can multiple, Video tags
+        [[ "$title" =~ (^|[[:space:]])live($|[[:space:]]) ]] && echo "  <tag>live</tag>"
+        [[ "$title" =~ lyric ]] && echo "  <tag>lyrics</tag>"
+        [[ "$title" =~ (^|[[:space:]])remix($|[[:space:]]) ]] && echo "  <tag>remix</tag>"
+    } >> "$nfofile"
 #   echo "  <director></director>" >> ${nfofile}        # can multiple, Director of the music video
 #   echo "  <premiered></premiered>" >> ${nfofile}      # Release date, Format as yyyy-mm-dd
 #   echo "  <year></year>" >> ${nfofile}                # Release Year, Note: Kodi v20: Use <premiered> tag only
@@ -87,15 +111,47 @@ for infile in *.mp4; do
 #   echo "  <actor><name>Taylor Swift</name><role></role><order>0</order><thumb></thumb></actor>" >> ${nfofile} # can multiple, The artist of the song
 #   echo "  <dateadded></dateadded>" >> ${nfofile}      # mTime of the playable video file
     if [ -z "$artist" ]; then
-		echo "  <artist></artist>" >> ${nfofile}        # unidentified delimiter "-", write valueless
+		echo "  <artist></artist>" >> "$nfofile"         # unidentified delimiter "-", write valueless
     else
-        echo "  <artist>${artist}</artist>" | sed -r 's/[[:blank:]]+/ /g' >> ${nfofile} # can multiple, The artists/actors in the music video
+        artists_xml=$(awk -v artist="$artist" -v title="$title" '
+            BEGIN {
+                artists_raw = artist
+                title_tail = title
+                IGNORECASE = 1
 
-		# todo: split multiple artists/actors
-		# (separators: & and , ft. feat. pres. vs.), cut leading/tailing space, write multiple artist lines
-		# $title can have "(feat. artistname)"
+                while (match(title_tail, /\([^)]*\)/)) {
+                    paren = substr(title_tail, RSTART + 1, RLENGTH - 2)
+                    if (match(paren, /(^|[[:space:]])(feat\.?|ft\.?)[[:space:]]+/)) {
+                        artists_raw = artists_raw "," substr(paren, RSTART + RLENGTH)
+                    }
+                    title_tail = substr(title_tail, RSTART + RLENGTH)
+                }
+
+                gsub(/[[:space:]]+/, " ", artists_raw)
+                gsub(/[[:space:]]*&[[:space:]]*/, "\n", artists_raw)
+                gsub(/[[:space:]]*,[[:space:]]*/, "\n", artists_raw)
+                gsub(/[[:space:]]+(ft\.?|feat\.?|pres\.?|vs\.?|and)[[:space:]]+/, "\n", artists_raw)
+
+                count = split(artists_raw, arr, /\n/)
+                for (i = 1; i <= count; i++) {
+                    item = arr[i]
+                    gsub(/^[[:space:]]+|[[:space:]]+$/, "", item)
+                    if (item == "") continue
+                    key = tolower(item)
+                    if (!seen[key]++) print item
+                }
+            }
+        ')
+        if [ -n "$artists_xml" ]; then
+            while IFS= read -r artist_name; do
+                artist_name="$(xml_escape "$artist_name")"
+                echo "  <artist>${artist_name}</artist>" >> "$nfofile"
+            done <<< "$artists_xml"
+        else
+            echo "  <artist></artist>" >> "$nfofile"
+        fi
     fi
-    echo "</musicvideo>" >> ${nfofile}                  # close top level parent tag
+    shopt -u nocasematch
+    echo "</musicvideo>" >> "$nfofile"                  # close top level parent tag
     echo "-------"
 done
-
